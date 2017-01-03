f3531351bee5c3770b992392573a5b263ba5bfcb
Darren Torpey <darren.torpey@gmail.com>
2017-01-03 00:36:16 -0500

    Concept proven

--- src/app.js
+++ src/app.js
@@ -51,6 +51,10 @@ function greenBlast() {
 document.addEventListener('click', function(e) {
   const newStarburst = new Starburst();
   newStarburst.moveToCoords({ x: e.pageX, y: e.pageY });
+  if (window.clock) {
+    var time = window.clock.context.currentTime.toString().match(/[0-9]+.?[0-9]{0,2}/);
+    console.log('Time was', time[0]);
+  }
 }, false);
 
 window.addEventListener('keydown', function(e) {
@@ -73,3 +77,52 @@ window.addEventListener('keydown', function(e) {
       break;
   }
 });
+
+function getData(url) {
+  var audioCtx = new AudioContext();
+  var source = audioCtx.createBufferSource();
+  var request = new XMLHttpRequest();
+
+  request.open('GET', url, true);
+
+  request.responseType = 'arraybuffer';
+
+  request.onload = function() {
+    var audioData = request.response;
+
+    audioCtx.decodeAudioData(audioData, function(buffer) {
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+      },
+
+      function(e){"Error with decoding audio data" + e.err});
+
+  }
+
+  request.send();
+
+  return source;
+}
+
+function getAudioClock(url) {
+    var source = getData(url);
+    var clock = new WAAClock(source.context);
+    clock.start();
+    source.start();
+    return clock;
+}
+
+window.clock = getAudioClock('http://localhost:3000/sound/first_breath.mp3');
+
+function atTime(time, action) {
+  window.clock.callbackAtTime(action, time);
+}
+
+atTime(  1.00, WaterBurst.play );
+atTime(  4.88, WaterBurst.play );
+atTime(  8.83, WaterBurst.play );
+atTime( 12.63, WaterBurst.play );
+atTime( 16.63, WaterBurst.play );
+atTime( 20.38, WaterBurst.play );
+atTime( 24.55, WaterBurst.play );
+atTime( 28.48, WaterBurst.play );
