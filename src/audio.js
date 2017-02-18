ebbe1fd8e2f964aebd1f61d0b33bb53ff082df9c
Darren Torpey <darren.torpey@gmail.com>
2017-02-18 17:08:01 -0500

    show.jumpTo!

--- src/audio.js
+++ src/audio.js
@@ -69,39 +69,8 @@ function getAudioClockFromUrl(url) {
   });
 }
 
-// window._song = document.querySelector('audio');
-// window.clock = getAudioClock({ el: _song });
-// window._song = document.querySelector('audio');
-// window._song.play();
-// window._songClock = new WAAClock(_song);
-// document.querySelector('audio').onload = function() {
-//   console.log('here, now');
-// };
-function getAudioClockFromElement(el) {
-  var audioCtx = new AudioContext();
-  window._audioCtx = audioCtx;
-  var mediaElementSource = audioCtx.createMediaElementSource(el);
-  window._mediaElementSource = mediaElementSource;
-  mediaElementSource.connect(audioCtx.destination)
-  var bufferSource = audioCtx.createBufferSource()
-  window._bufferSource = bufferSource;
-  // var buffAudio = new BuffAudio(audioCtx, bufferSource);
-  // window._buffAudio = buffAudio;
-  // bufferSource.connect(audioCtx.destination);
-
-  el.play();
-  var clock = new WAAClock(mediaElementSource.context);
-  clock.start();
-
-  window._newSource = mediaElementSource;
-
-  return clock;
-}
-
 export function getAudioClock(opts = {}) {
   if (opts.url) {
     return getAudioClockFromUrl(opts.url);
-  } else if (opts.el) {
-    return getAudioClockFromElement(opts.el);
   }
 }
