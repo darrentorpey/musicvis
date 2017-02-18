a1fd06aab5bcf8765437106d5c5ee554857580cb
Darren Torpey <darren.torpey@gmail.com>
2017-02-18 15:33:27 -0500

    multi-action syntax

--- src/app.js
+++ src/app.js
@@ -49,15 +49,25 @@ function startShow({ song, clock }) {
     clock.callbackAtTime(action, time);
   }
 
+  function parseActions(action) {
+    if (_.isFunction(action)) {
+      return [action];
+    } else {
+      let actions = action.split('|');
+      return actions.map(action => actionShortcuts[action]);
+    }
+  }
+
   function times(timedData) {
     for (let [time, action] of timedData) {
-      atTime(time, action);
+      let actions = parseActions(action);
+      actions.forEach(action => atTime(time, action));
     }
   }
 
   times([
     [   1.00, 'boom_lightblue'  ],
-    [   4.88, 'boom_blue'   ],
+    [   4.88, 'boom_blue' ],
     [   8.83, 'boom_blue' ],
     [  12.63, 'boom_blue'   ],
     [  16.63, 'boom_lightblue'  ],
@@ -72,5 +82,12 @@ function startShow({ song, clock }) {
     [  34.35, 'boom_blue'  ],
     [  35.33, 'boom_blue'  ],
     [  36.28, 'boom_lightblue'  ],
+    [  24.40, 'water|water|water|water|water' ],
+    [  24.90, 'water|water|water|water|water' ],
+    [  25.40, 'water|water|water|water|water' ],
+    [  25.90, 'water|water|water|water|water' ],
+    [  26.40, 'water|water|water|water|water' ],
+    [  26.90, 'water|water|water|water|water' ],
+    [  27.40, 'water|water|water|water|water' ],
   ]);
 }
