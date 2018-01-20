8914a6d08872b2d4450bd15db048e7d0b6ad06a9
Darren Torpey <darren.torpey@gmail.com>
2018-01-20 12:37:53 -0500

    Better controls code

--- src/controls.js
+++ src/controls.js
@@ -3,34 +3,19 @@ import { BubbleField, Effects, Starburst } from './effects'
 const actions = {
   m: () => window._show.toggleMute(),
   t: () => console.log('Time was', window._show.getCurrentTime()),
+  g: () => BubbleField.play({ y: window.innerHeight * 0.8 }),
+  x: () => Effects.blueBlast(),
+  c: () => Effects.greenBlast(),
+  w: () => Effects.waterBurst(),
+  v: () => Effects.lightBlueBlast(),
+  z: () => Effects.orangeBlast(),
 }
 
 export function bindToKeys() {
   window.addEventListener('keydown', function(e) {
-    let action
-    switch (e.key) {
-      case 'z':
-        Effects.orangeBlast()
-        break
-      case 'x':
-        Effects.blueBlast()
-        break
-      case 'c':
-        Effects.greenBlast()
-        break
-      case 'w':
-        Effects.waterBurst()
-        break
-      case 'v':
-        Effects.lightBlueBlast()
-        break
-      case 'g':
-        BubbleField.play({ y: window.innerHeight * 0.8 })
-        break
-      default:
-        action = actions[e.key]
-        return action ? action() : null
-    }
+    const action = actions[e.key]
+
+    action ? action() : null
   })
 }
 
